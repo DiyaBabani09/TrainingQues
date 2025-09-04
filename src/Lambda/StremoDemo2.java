4795aea02f2bb56f0021ebf92ec9675272596328
DiyaBabani09 <diyababani03@gmail.com>
2025-09-04 18:48:11 +0530

    Intial commit

--- src/Lambda/StremoDemo2.java
+++ src/Lambda/StremoDemo2.java
@@ -8,6 +8,7 @@ public class StremoDemo2 {
     public static void main(String[] args) {
         List<Integer> a= Arrays.asList(1,2,3,3);
         System.out.println(  a.stream().skip(2).toList());
+        a.forEach(System.out::println);
 
     }
 }
